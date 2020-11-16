930b8f57685028a26653d576ba044f06fc08dbda
Byron Jimenez <byronjimenez9911@gmail.com>
2020-11-16 09:07:33 -0600

    JSDoc and export module

--- validation.js
+++ validation.js
@@ -1,93 +1,108 @@
+/**
+ * @file Simple validations: numeric, max, min, greatherThan, equalTo, required
+ * @author Byron Jimenez
+ * @version 0.1
+ */
+
 class Validation {
 
-   constructor(rules, object) {
-       this.rules = rules;
-       this.object = object;
-       this.errors = [];
+    /**
+     * @param {Array} rules
+     * @param {Object} object
+     * @description vcontructor.
+     */
+    constructor(rules, object) {
+        this.rules = rules;
+        this.object = object;
+        this.errors = [];
 
-       this.defaultMessages = {
-           numeric : (field) => `${field} is not numeric`,
-           max : (field, value) => `${field} value is greather than ${value}`,
-           min : (field, value) => `${field} value is greather than ${value}`,
-           greaterThan : (field, value) => `${field} value is not greather than ${value}`,
-           equalTo : (field, value) => `${field} value is not equal to ${value}`,
-           required : (field) => `${field} is required`
-       }
+        this.defaultMessages = {
+            numeric : (field) => `${field} is not numeric`,
+            max : (field, value) => `${field} value is greather than ${value}`,
+            min : (field, value) => `${field} value is greather than ${value}`,
+            greaterThan : (field, value) => `${field} value is not greather than ${value}`,
+            equalTo : (field, value) => `${field} value is not equal to ${value}`,
+            required : (field) => `${field} is required`
+        }
 
-   }
+    }
 
-   validate () {
-       for (const item of this.rules) {
-           if (item.hasOwnProperty("field") && item.hasOwnProperty("validate")) {
+    /**
+     * @returns {Object}
+     * @description validate object values ​​according to rules.
+     */
+    validate () {
+        for (const item of this.rules) {
+            if (item.hasOwnProperty("field") && item.hasOwnProperty("validate")) {
 
-               if (item.validate.hasOwnProperty("type")) {
+                if (item.validate.hasOwnProperty("type")) {
 
-                   const fieldName = item.field
+                    const fieldName = item.field
 
-                   if (this.object.hasOwnProperty(fieldName)) {
+                    if (this.object.hasOwnProperty(fieldName)) {
 
-                       this.currentField = this.object[fieldName]
-                       this.currentValType = item.validate.type
-                       this.currentMessage = item.validate.message;
-                       this.currentValue = item.validate.value;
+                        this.currentField = this.object[fieldName]
+                        this.currentValType = item.validate.type
+                        this.currentMessage = item.validate.message;
+                        this.currentValue = item.validate.value;
 
-                       switch (this.currentValType) {
-                           case "numeric":
+                        switch (this.currentValType) {
+                            case "numeric":
 
-                               if (!this.validateNumeric()) {
-                                   this.setError()
-                               }
+                                if (!this.validateNumeric()) {
+                                    this.setError()
+                                }
 
-                           case "max":
+                            case "max":
 
-                               if (!this.validateMax()) {
-                                   this.setError()
-                               }
+                                if (!this.validateMax()) {
+                                    tthis.setError()
+                                }
 
-                           case "min":
+                            case "min":
 
-                               if (!this.validateMin()) {
-                                   this.setError()
-                               }
+                                if (!this.validateMin()) {
+                                    this.setError()
+                                }
 
-                           case "greaterThan":
+                            case "greaterThan":
 
-                               if (!this.validateGreaterThan()) {
-                                   this.setError()
-                               }
+                                if (!this.validateGreaterThan()) {
+                                    this.setError()
+                                }
 
-                           case "equalTo":
+                            case "equalTo":
 
-                               if (!this.validateEqualTo()) {
-                                   this.setError()
-                               }
+                                if (!this.validateEqualTo()) {
+                                    this.setError()
+                                }
 
-                           case "required":
+                            case "required":
 
-                               if (!this.validateRequired()) {
-                                   this.setError()
-                               }
+                                if (!this.validateRequired()) {
+                                    this.setError()
+                                }
 
-                           default:
-                               console.warn("Validation type does not exist")
-                       }
+                            default:
+                                console.warn("Validation type does not exist")
+                        }
 
-                   } else {
-                       console.error(`Field ${field} does not exist`)
-                   }
+                    } else {
+                        console.error(`Field ${field} does not exist`)
+                    }
 
-               } else {
-                   console.error("Validation type is required")
-               }
+                } else {
+                    console.error("Validation type is required")
+                }
 
-           } else {
-               console.error("Field and validate are required")
-           }
-       }
-   }
+            } else {
+                console.error("Field and validate are required")
+            }
+        }
+    }
 
-   setError() {
-    this.errors.push(this.setMessage())
+    setError() {
+        this.errors.push(this.setMessage())
     }
 
     setMessage() {
@@ -146,7 +161,7 @@ class Validation {
         }
     }
 
-}
+ }
 
 
-
+export default Validation;
